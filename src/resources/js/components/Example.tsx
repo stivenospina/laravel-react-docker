ea25e44e5bcb735ea861ae9d00489d85adc05035
yanaguilar <ianaguilar9110@gmail.com>
2023-04-03 16:43:09 +0900

    fix title name....etc

--- src/resources/js/components/Example.tsx
+++ src/resources/js/components/Example.tsx
@@ -66,9 +66,9 @@ const Example  = () => {
             <div className="row justify-content-center">
                 <div className="col-md-8">
                     <div className="card">
-                        <div className="card-header">Example Component</div>
+                        <div className="card-header"><h1>掲示板風メモアプリ</h1></div>
 
-                        <div className="card-body">I'm an example component!</div>
+                        <div className="card-body">メモをとろうぜ！！</div>
                     </div>
                 </div>
             </div>
@@ -136,9 +136,9 @@ const Example  = () => {
             <div className="row justify-content-center">
                 <div className="col-md-8">
                     <div className="card">
-                        <div className="card-header">Example Component</div>
+                        <div className="card-header"><h1>掲示板風メモアプリ</h1></div>
 
-                        <div className="card-body">I'm an example component!</div>
+                        <div className="card-body">メモをとろうぜ！！</div>
                     </div>
                 </div>
             </div>
